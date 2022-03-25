55b87fdb59087dba533d4ac8dff3435f7e34a070
jsrankin-com <89367845+jsrankin-com@users.noreply.github.com>
2022-03-25 04:12:38 -0400

    Update stopwatch.cpp
    Overloaded operator <<

--- stopwatch.cpp
+++ stopwatch.cpp
@@ -5,7 +5,7 @@
    (_.'_..--.._'._)         Creates a stopwatch object that works as a timer.
     /.' . 60 . '.\      There are 3 public functions to Start(), Stop(),
    // .      / . \\     and Seconds() which reports the total elapsed seconds.
-  |; .      /   . |;
+  |; .      /   . |;    
   ||45    ()    15||
   |; .          . |;
    \\ .        . //
@@ -16,6 +16,7 @@
 
 #include "stopwatch.h"
 namespace CHRONO = std::chrono;
+
 Stopwatch::Stopwatch()
 {
     running = false;
@@ -24,14 +25,14 @@ Stopwatch::Stopwatch()
     time_span = end - beg;
 }
 
-double Stopwatch::Seconds()
+double Stopwatch::Seconds() const
 {
     //Ensure we report the running time if requested
     return (!running)
-        ? double(time_span.count()) *
-        CHRONO::steady_clock::period::num / CHRONO::steady_clock::period::den
-        : double((CHRONO::steady_clock::now() - beg).count()) *
-        CHRONO::steady_clock::period::num / CHRONO::steady_clock::period::den;
+        ? static_cast<double>(time_span.count()) *
+         CHRONO::steady_clock::period::num / CHRONO::steady_clock::period::den
+        : static_cast<double>((CHRONO::steady_clock::now() - beg).count()) *
+         CHRONO::steady_clock::period::num / CHRONO::steady_clock::period::den;
 }
 
 void Stopwatch::Start()
@@ -45,6 +46,12 @@ void Stopwatch::Stop()
     end = CHRONO::steady_clock::now();
     time_span = end - beg;
     running = false;
+}
+
+std::ostream& operator <<(std::ostream& os, const Stopwatch& t)
+{
+    os << std::format("Seconds: {}\n", t.Seconds());
+    return os;
 }
 
 /*
